e1f75a795ede4d16d49efe0ca5550dd8514dafb3
Dr Christis Katsouris <43221582+christiskatsouris@users.noreply.github.com>
2022-11-24 15:02:38 +0200

    Update tsbootstrap.R

--- R Code/tsbootstrap.R
+++ R Code/tsbootstrap.R
@@ -1,5 +1,8 @@
-
-tsbootstrap <- function(data, ...) {
+// R function 
+  
+  
+tsbootstrap <- function(data, ...) 
+{# begin-of-function
   UseMethod("tsbootstrap")
 }
 
@@ -8,20 +11,24 @@ TSBOOT_TYPES <- c("fixed", "geom")
 #' @describeIn tsbootstrap Generate time series replicates using blocks of
 #'   rows in the data frame.
 #' @export
-tsbootstrap.data.frame <- function(data, R = 1L, size = 1L, m = nrow(data),
-                                   type = "fixed", ...) {
+
+tsbootstrap.data.frame <- function(data, R = 1L, size = 1L, m = nrow(data), type = "fixed", ...) 
+{# begin-of-function
+  
   assert_that(is.number(R) && R >= 1)
   assert_that(is.number(size) && size >= 1L && size <= nrow(data))
   assert_that(type %in% TSBOOT_TYPES)
-  to_resample_df(tsbootstrap_(nrow(data), R = R, size = size, type = type),
-                 data)
-}
+  to_resample_df(tsbootstrap_(nrow(data), R = R, size = size, type = type), data)
+
+}# end-of-function
 
 #' @describeIn tsbootstrap Generate time series replicates using blocks of
 #'   groups in the grouped data frame.
 #' @export
-tsbootstrap.grouped_df <- function(data, R = 1L, size = 1L, type = "fixed",
-                                   m = dplyr::n_groups(data), ...) {
+
+tsbootstrap.grouped_df <- function(data, R = 1L, size = 1L, type = "fixed", m = dplyr::n_groups(data), ...)
+{# begin-of-function
+  
   assert_that(is.number(R) && R >= 1)
   assert_that(is.number(size) && size >= 1L && size <= nrow(data))
   assert_that(type %in% TSBOOT_TYPES)
@@ -30,7 +37,8 @@ tsbootstrap.grouped_df <- function(data, R = 1L, size = 1L, type = "fixed",
   res <- mutate_(tsbootstrap_(length(idx), R = R, size = size, type = type),
                  sample = ~ map(sample, f))
   to_resample_df(res, data)
-}
+
+}# end-of-function
 
 # Adapted from boot:::make.ends
 mod <- function(i, n, endcorr) {
@@ -40,21 +48,30 @@ mod <- function(i, n, endcorr) {
 
 # Fixed Moving Block Bootstrap from boot::tsboot and boot:::ts.array
 # Fixed Moving Block Bootstrap from boot::tsboot
-.tsboot_mbb <- function(n, m = n, size = 1L, endcorr = TRUE) {
+
+.tsboot_mbb <- function(n, m = n, size = 1L, endcorr = TRUE)
+{# begin-of-function
+  
   endpt <- if (endcorr) n else n - size + 1L
   nn <- ceiling(m / size)
   lens <- c(rep(size, nn - 1L), 1L + (m - 1L) %% size)
   st <- sample.int(endpt, nn, replace = TRUE)
-  flatten_int(purrr::map2(st, lens, function(s, l) {
+ 
+  flatten_int(purrr::map2(st, lens, function(s, l)
+  {
     if (l >= 1) as.integer(mod(seq(s, s + l - 1L), n, endcorr))
     else integer()
   }))
-}
+
+}# end-of-function
 
 # Fixed Moving Block Bootstrap from boot::tsboot and boot:::ts.array
 # adapted from
 # use ... to ignore endcorr
-.tsboot_geom <- function(n, m = n, size = 1L, ...) {
+
+.tsboot_geom <- function(n, m = n, size = 1L, ...)
+{# begin-of-function
+  
   endpt <- n - size + 1L
   # worst case scenario is to draw m. So take m draws from
   # geom and truncate
@@ -63,19 +80,25 @@ mod <- function(i, n, endcorr) {
   # truncate to minimum length >= m
   lens <- lens[seq_len(purrr::detect_index(len_tot, ~ .x >= m))]
   st <- sample.int(endpt, length(lens), replace = TRUE)
-  f <- function(s, l) {
+  
+  f <- function(s, l) 
+  {
     if (l >= 1) as.integer(mod(seq.int(s, s + l - 1L), n, TRUE))
     else integer()
   }
   utils::head(flatten_int(purrr::map2(st, lens, f)), m)
-}
 
-tsbootstrap_ <- function(n, R = 1L, m = n, size = 1L, type = "fixed",
-                         endcorr = TRUE) {
+}# end-of-function
+
+
+tsbootstrap_ <- function(n, R = 1L, m = n, size = 1L, type = "fixed", endcorr = TRUE) 
+{# begin-of-function
+  
   f <- switch(type,
               geom = .tsboot_geom,
               fixed = .tsboot_mbb,
               stop("type = ", type, " is not recognized.", call. = FALSE))
-  tibble(sample = purrr::rerun(R, f(n, m = m, size = size, endcorr = endcorr)),
-         .id = seq_len(R))
-}
+  
+  tibble(sample = purrr::rerun(R, f(n, m = m, size = size, endcorr = endcorr)), .id = seq_len(R))
+
+}# end-of-function
